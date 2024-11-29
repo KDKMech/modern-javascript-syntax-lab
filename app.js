d082aaf7eb9c69a44cf5ccb21ca70449dacad724
milestarricone <milestarricone2@gmail.com>
2024-11-29 16:27:12 -0500

    HW complete. Good excersise

--- app.js
+++ app.js
@@ -50,6 +50,121 @@ console.log(model);
 
 // Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
 
-const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 // Your code here
+
+const controversialPizzaToppings = [...pizzaToppings]
+
+console.log(controversialPizzaToppings)
+
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
+
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+//   };
+  
+  // Change the `model` property of `myCar` to 'q7'. Log both objects.
+  
+  // Your code here
+  
+
+  const myCar = {...car}
+
+  myCar.model = `q7`
+
+  console.log(car);
+  console.log(myCar);
+
+  // Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+
+const propertyName = `username`
+
+const userProfile = {
+    [propertyName]: `Miles`,
+}
+
+console.log(userProfile);
+
+
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+
+const sentence = (noun = `cat`, adjective = `white`) => {
+    console.log(`The ${noun} is ${adjective}.`);
+    
+}
+sentence()
+
+
+// Convert the following `if...else` statement in to a ternary:
+
+let pizza = 'tasty';
+
+// if (pizza === 'tasty') {
+//   console.log('yum');
+// } else {
+//   console.log('yuck');
+// }
+
+let good = pizza === `tasty` ? `yum` : `yuck`;
+// Your code here
+console.log(good);
+
+
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+
+const LANG = localLangConfig || `en`
+
+// Log the result
+console.log('Language setting:', LANG);
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+
+let USER_THEME = userSavedTheme || `light`
+
+// Log the result
+console.log('User theme setting:', USER_THEME);
+
+
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+    name: 'Alice',
+  };
+  
+  let cat = adventurer.cat?.name// Your code here
+  
+  console.log(cat);
+  
---
+++ exportingFile.js
@@ -0,0 +1,3 @@
+export default `Miles`;
+export const age = 31;
+export const job = `developer`;
